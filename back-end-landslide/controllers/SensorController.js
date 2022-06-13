13494d20c00cceeed69565e039e2acd544aa5269
doanduchieupw <doanduchieu.pw12@gmail.com>
2022-06-14 00:18:35 +0700

    minor change

--- back-end-landslide/controllers/SensorController.js
+++ back-end-landslide/controllers/SensorController.js
@@ -3,8 +3,9 @@ const Sensor = require('../models/Sensor');
 const SensorController = {
     getData: async(req, res) => {
         try {
-            const user = await Sensor.find().sort({ _id: -1 }).limit(10);
-            return res.status(200).json(user)
+            const sensor = await Sensor.find().sort({ _id: -1}).limit(10);
+            
+            return res.status(200).json(sensor.reverse());
         } catch (err) {
             return res.status(500).json({err})
         }
